63f7722ec141139affcde4d42857e3ba9b214349
fedodd <fedodd.s@gmail.com>
2020-03-22 13:12:23 +0300

    restyle

--- src/App.js
+++ src/App.js
@@ -10,52 +10,44 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      style: {
-      "--paddingSide": "15px",
-	    "--paddingTop": "22px",
-      "--paddingSideSmall": "8px",
-      "--paddingTopSmall": "11px"}};
+					styleOn: true,
+					style: {
+						"--paddingSide": "2%",
+						"--paddingTop": "2%",
+						"--paddingSideSmall": "0",
+						"--paddingTopSmall": "15px"
+					}
+				};
   }
-
-
-  onClickHandler(e) {
-    const style = {
-      "--paddingSide": "15px",
-	    "--paddingTop": "22px",
-      "--paddingSideSmall": "8px",
-      "--paddingTopSmall": "11px"};
-
-    (this.state.style === null) ? this.setState({style }) : this.setState({style: null });
-
-  }
-
 
   render() {
+    const style = this.state.styleOn ? this.state.style : null;
     return (
-      <div className={classes.App} style={this.state.style}>
-
-        <Header />
-        <div className={classes.content}>
-          <Block size="is__banner"/>
-          <div className={classes.blockWrapper}>
-            <Block size="is__square"/>
-            <Block size="is__square"/>
-          </div>
-          <Block size="is__fullscreen"/>
-          <div className={classes.blockWrapper}>
-            <Block size="is__high"/>
-            <Block size="is__high"/>
-          </div>
-          <Block size="is__fullscreen"/>
-        </div>
-        <div>
-          <button
-            onClick={(e) =>this.onClickHandler(e)}
-            style={{position: 'fixed', top: '10px', right: '10px'}}>Переключить отступы</button>
-        </div>
-      </div>
-
-    );
+					<div className={classes.App} style={style}>
+						<Header />
+						<div className={classes.content}>
+							<Block size="is__banner" />
+							<div className={classes.blockWrapper}>
+								<Block size="is__square" />
+								<Block size="is__square" />
+							</div>
+							<Block size="is__fullscreen" />
+							<div className={classes.blockWrapper}>
+								<Block size="is__high" />
+								<Block size="is__high" />
+							</div>
+							<Block size="is__fullscreen" />
+						</div>
+						<div>
+							<button
+								onClick={(e) => this.setState({ styleOn: !this.state.styleOn })}
+								style={{ position: "fixed", top: "10px", right: "10px" }}
+							>
+								Переключить отступы
+							</button>
+						</div>
+					</div>
+				);
   }
 }
 
