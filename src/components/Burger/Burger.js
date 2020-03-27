ae858e9d444b451ffcc7affdc24975d4a2769e88
fedodd <fedodd.s@gmail.com>
2020-03-27 21:02:02 +0300

    style burger menu

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,29 @@ import classes from "./Burger.module.pcss";
 
 const Burger = ({ open, setOpen }) => {
 
-  const transitionStyle = open
-			? {
-					transition:
-						"top 0.3s linear, bottom 0.3s linear, transform 0.3s linear 0.2s"
-			  }
-			: {
-					transition:
-						"top 0.3s linear 0.2s, bottom 0.3s linear 0.2s, transform 0.3s linear"
-			  };
+  const transitionStyleTop = open
+    ? {
+        transform: "rotate(45deg)",
+        top: "15px",
+        transition: "top 0.3s linear, transform 0.3s linear 0.2s"
+      }
+    : {
+        transform: "rotate(0)",
+        top: "10px",
+        transition: "top 0.3s linear 0.2s, transform 0.3s linear"
+      };
+
+  const transitionStyleBottom = open
+      ? {
+          transform: "rotate(-45deg)",
+          bottom: "14px",
+          transition: "bottom 0.3s linear, transform 0.3s linear 0.2s"
+        }
+      : {
+          transform: "rotate(0)",
+          bottom: "12px",
+          transition: "bottom 0.3s linear 0.2s, transform 0.3s linear"
+        };
 
   return (
 			<button
@@ -19,21 +33,9 @@ const Burger = ({ open, setOpen }) => {
 				onClick={() => setOpen(!open)}
 				className={classes.burger}
 			>
-				<div
-					style={
-						open
-							? { transform: "rotate(45deg)", top: "15px", ...transitionStyle }
-							: { transform: "rotate(0)", top: "10px", ...transitionStyle }
-					}
-				/>
+				<div style={transitionStyleTop} />
 				{/* <div style={open ? {opacity: '0', transform: 'rotate(45deg)'} : {opacity: '1', transform: 'rotate(0)'}}/> */}
-				<div
-					style={
-						open
-							? { transform: "rotate(-45deg)", bottom: "14px", ...transitionStyle }
-							: { transform: "rotate(0)", bottom: "12px", ...transitionStyle }
-					}
-				/>
+				<div style={transitionStyleBottom} />
 			</button>
 		);
 }
