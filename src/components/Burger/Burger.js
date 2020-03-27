964155c6218cfc0c3527b439115488a08d08f98b
fedodd <fedodd.s@gmail.com>
2020-03-27 20:45:04 +0300

    style burger lines like at apple

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,17 +3,39 @@ import classes from "./Burger.module.pcss";
 
 const Burger = ({ open, setOpen }) => {
 
+  const transitionStyle = open
+			? {
+					transition:
+						"top 0.3s linear, bottom 0.3s linear, transform 0.3s linear 0.2s"
+			  }
+			: {
+					transition:
+						"top 0.3s linear 0.2s, bottom 0.3s linear 0.2s, transform 0.3s linear"
+			  };
+
   return (
-    <button
-      open={open}
-      onClick={() => setOpen(!open)}
-      // style={open ? {background: '#0D0C1D'} : {background: '#EFFFFA'}}
-      className={classes.burger}>
-      <div style={open ? {transform: 'rotate(45deg)', top: '15px'} : {transform: 'rotate(0)', top: '12px'}}/>
-      {/* <div style={open ? {opacity: '0', transform: 'rotate(45deg)'} : {opacity: '1', transform: 'rotate(0)'}}/> */}
-      <div style={open ? {transform: 'rotate(-45deg)', bottom: '14px'} : {transform: 'rotate(0)', bottom: '12px'}}/>
-    </button>
-  )
+			<button
+				open={open}
+				onClick={() => setOpen(!open)}
+				className={classes.burger}
+			>
+				<div
+					style={
+						open
+							? { transform: "rotate(45deg)", top: "15px", ...transitionStyle }
+							: { transform: "rotate(0)", top: "10px", ...transitionStyle }
+					}
+				/>
+				{/* <div style={open ? {opacity: '0', transform: 'rotate(45deg)'} : {opacity: '1', transform: 'rotate(0)'}}/> */}
+				<div
+					style={
+						open
+							? { transform: "rotate(-45deg)", bottom: "14px", ...transitionStyle }
+							: { transform: "rotate(0)", bottom: "12px", ...transitionStyle }
+					}
+				/>
+			</button>
+		);
 }
 
 export default Burger;
