cc964f6b9eb13ce737e9678a43cf633be2750642
fedodd <fedodd.s@gmail.com>
2020-04-01 15:28:18 +0300

    start header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,15 @@ import HeaderItem from "../HeaderItem/HeaderItem";
 import Burger from "../Burger/Burger";
 import Menu from "../Menu/Menu";
 import logo from "../../images/icon.svg";
+// import flat from "../../images/flat.png"
 
 import classes from "./header.module.pcss";
 
 function Header(props) {
-  const [open, setOpen] = React.useState(false);
-  const node = React.useRef();
-
-  const [isHidden, setHidden] = React.useState(false);
-
-  const size = useWindowSize();
+  // const [open, setOpen] = React.useState(false);
+  // const node = React.useRef();
+  // const [isHidden, setHidden] = React.useState(false);
+  // const size = useWindowSize();
 
   // Hook
   function useWindowSize() {
@@ -33,25 +32,53 @@ function Header(props) {
     return windowSize;
   }
 
+  const [product, setProduct] = React.useState(null);
+
+  let RLX10 = {
+    name: 'RLX10',
+    price: 89
+  }
+
   return (
-    <header className={classes.header}  style={open ? {borderBottom: '1px solid rgb(66, 66, 69)'} : {borderBottom: 'none'} }>
-      <div className={classes.headerHolder}>
-        <img src={logo} className={classes.logo} />
-        <div ref={node} className={classes.burgerWrapper}>
-          <Burger open={open} setOpen={setOpen} />
-          <Menu open={open} setOpen={setOpen} />
+       <header className={classes.header}>
+        <div className={classes.headerHolder}>
+          <img src={logo} className={classes.logo} />
+          <nav className={classes.nav}>
+            <li className={classes.product}>{product ? product.name : 'Products'}
+              <div className={classes.productList}>
+                <button className={classes.button} onClick={()=> setProduct(RLX10)}>RLX10</button>
+              </div>
+            </li>
+            {product ? <React.Fragment>
+                <li>Overview</li>
+                <li>Why {product.name}</li>
+                <li>Tech Specs</li>
+                <li><button className={classes.button}>{button.price}$</button></li>
+              </React.Fragment>: null}
+          </nav>
+
+
         </div>
-      </div>
-      <div className={classes.headerHolder2} style={isHidden ? {display: 'none'} : {display: 'block'}}>
-        <ul>
-          <li className={classes.is__big}>iOS Design Kit</li>
-          <li>How it works</li>
-          <li>What&rsquo;s inside</li>
-          <li>iOS 13 GUI<span>free</span></li>
-          <li><button className={classes.buttonAlt} onClick={()=> setHidden(true)}>Buy now</button></li>
-        </ul>
-      </div>
-    </header>
+
+       </header>
+    // <header className={classes.header}  style={open ? {borderBottom: '1px solid rgb(66, 66, 69)'} : {borderBottom: 'none'} }>
+    //   <div className={classes.headerHolder}>
+    //     <img src={logo} className={classes.logo} />
+    //     <div ref={node} className={classes.burgerWrapper}>
+    //       <Burger open={open} setOpen={setOpen} />
+    //       <Menu open={open} setOpen={setOpen} />
+    //     </div>
+    //   </div>
+    //   <div className={classes.headerHolder2} style={isHidden ? {display: 'none'} : {display: 'block'}}>
+    //     <ul>
+    //       <li className={classes.is__big}>iOS Design Kit</li>
+    //       <li>How it works</li>
+    //       <li>What&rsquo;s inside</li>
+    //       <li>iOS 13 GUI<span>free</span></li>
+    //       <li><button className={classes.buttonAlt} onClick={()=> setHidden(true)}>Buy now</button></li>
+    //     </ul>
+    //   </div>
+    // </header>
   );
 }
 
