5463ceb66aa9a3d19b022c86d56115d1108d41b1
fedodd <fedodd.s@gmail.com>
2020-04-06 09:21:53 +0300

    change header elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,14 +20,14 @@ function Header(props) {
     const [windowSize, setWindowSize] = useState(window.innerWidth);
 
     useEffect(() => {
-					function handleResize() {
-						setWindowSize(window.innerWidth);
-					}
-					windowSize > 730 ? setOpen(true) : setOpen(false);
-					window.addEventListener("resize", handleResize);
+      function handleResize() {
+        setWindowSize(window.innerWidth);
+      }
+      windowSize > 730 ? setOpen(true) : setOpen(false);
+      window.addEventListener("resize", handleResize);
 
-					return () => window.removeEventListener("resize", handleResize);
-				}, [windowSize]); // Empty array ensures that effect is only run on mount and unmount
+      return () => window.removeEventListener("resize", handleResize);
+    }, [windowSize]); // Empty array ensures that effect is only run on mount and unmount
 
     return windowSize;
   }
@@ -36,36 +36,74 @@ function Header(props) {
 
   function handleProductClick(e) {
     e.preventDefault();
-    setProduct(RLX10);
-  }
-
-  let RLX10 = {
-    name: 'RLX10',
-    price: 89
+    setProduct({ name: e.target.value });
   }
 
   return (
-       <header className={classes.header}>
-        <div className={classes.headerHolder}>
-          <img src={logo} className={classes.logo} />
+    <header className={classes.header}>
+      <div className={classes.headerHolder}>
+        <img
+          src={logo}
+          className={classes.logo}
+          onClick={() => setProduct(null)}
+        />
+        {product ? (
+          <nav className={classes.nav + " " + classes.is__product}>
+            <li>{product.name}</li>
+            <li>Overview</li>
+            <li>Why {product.name}</li>
+            <li>Tech Specs</li>
+            <li className={classes.is__alt}>
+              <button className={classes.button}>Buy now</button>
+            </li>
+          </nav>
+        ) : (
           <nav className={classes.nav}>
-            <li className={classes.product}>{product ? product.name : 'Products'}
+            <li className={classes.product}>
+              Home
               <div className={classes.productList}>
-                <button className={classes.button} onClick={(e)=>handleProductClick(e)} style={{position: 'relative', top: '100px', left: '100px'}}>RLX10</button>
+                <input
+                  type="button"
+                  value="RLX100"
+                  className={classes.button}
+                  onClick={e => handleProductClick(e)}
+                  style={{ position: "relative", top: "100px", left: "100px" }}
+                ></input>
               </div>
             </li>
-            {product ? <React.Fragment>
-                <li>Overview</li>
-                <li>Why {product.name}</li>
-                <li>Tech Specs</li>
-                <li className={classes.is__alt}><button className={classes.button}>Buy now</button></li>
-              </React.Fragment>: <li  className={classes.is__alt}><button className={classes.button + ' ' + classes.is__alt}>Buy now</button></li>}
+            <li className={classes.product}>
+              Business
+              <div className={classes.productList}>
+                <input
+                  type="button"
+                  className={classes.button}
+                  value="UPL10"
+                  onClick={e => handleProductClick(e)}
+                  style={{ position: "relative", top: "50px", left: "200px" }}
+                ></input>
+              </div>
+            </li>
+            <li className={classes.product}>
+              Components
+              <div className={classes.productList}>
+                <input
+                  type="button"
+                  className={classes.button}
+                  value="SUD9"
+                  onClick={e => handleProductClick(e)}
+                  style={{ position: "relative", top: "60px", left: "50px" }}
+                ></input>
+              </div>
+            </li>
+            <li className={classes.is__alt}>
+              <button className={classes.button + " " + classes.is__alt}>
+                Buy now
+              </button>
+            </li>
           </nav>
-
-
-        </div>
-
-       </header>
+        )}
+      </div>
+    </header>
     // <header className={classes.header}  style={open ? {borderBottom: '1px solid rgb(66, 66, 69)'} : {borderBottom: 'none'} }>
     //   <div className={classes.headerHolder}>
     //     <img src={logo} className={classes.logo} />
